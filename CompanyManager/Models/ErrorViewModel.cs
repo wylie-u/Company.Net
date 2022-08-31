0e6aa6d9c05efc8348436cec0ff9e458f0fdb2df
wylie-u <wylieurig@gmail.com>
2022-08-31 12:11:04 -0600

    initial commit

---
+++ CompanyManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CompanyManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
